e2e890e702d9fc20bd714e12da723f051c8677a6
loftum <loftum@gmail.com>
2019-04-03 08:15:38 +0200

    VS 2019

--- vsmac/install.sh
+++ vsmac/install.sh
@@ -1,2 +1,5 @@
 cp -v ./ColorThemes/*.* ~/Library/VisualStudio/7.0/ColorThemes
 cp -v ./KeyBindings/*.* ~/Library/VisualStudio/7.0/KeyBindings
+
+cp -v ./ColorThemes/*.* ~/Library/VisualStudio/8.0/ColorThemes
+cp -v ./KeyBindings/*.* ~/Library/VisualStudio/8.0/KeyBindings
